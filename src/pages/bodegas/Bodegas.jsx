5326c62ff0a19340984fa570d31f6529afdecc49
Javier López <javierpitino@gmail.com>
2022-12-07 21:24:49 +0100

    wineries some styles

--- src/pages/bodegas/Bodegas.jsx
+++ src/pages/bodegas/Bodegas.jsx
@@ -1,62 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { bodegasListService } from '../../services/bodegas.services';
+import React, { useEffect, useState } from "react";
+import "./bodegas.css";
+import { Link, useNavigate } from "react-router-dom";
+import { bodegasListService } from "../../services/bodegas.services";
 
 function Bodegas() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // 1. crear estados
-  const [ bodegas, setBodegas ] = useState([]);
+  const [bodegas, setBodegas] = useState([]);
 
   // 2. Component Did Mount
   useEffect(() => {
-    getAllBodegas()
-  }, [])
+    getAllBodegas();
+  }, []);
 
   const getAllBodegas = async () => {
-
     try {
       const response = await bodegasListService();
-      console.log(response.data)
-      setBodegas(response.data)
-
-    } catch(err) {
+      console.log(response.data);
+      setBodegas(response.data);
+    } catch (err) {
       if (err.response.status === 401) {
         navigate("/login");
       } else {
         navigate("/error");
       }
     }
-  } 
+  };
 
   if (!bodegas) {
-    return <h3>...loading</h3>
+    return <h3>...loading</h3>;
   }
 
   return (
-    <div>
-      <h3 className='title'>Bodegas</h3>
-      {
-        bodegas.map((eachBodega) => {
-          return(
-            <div key={eachBodega._id}>
-              <div>
-                <img src={eachBodega.image} alt="imagen bodega" width={150} />
+    <div className="wineries" >
+      {bodegas.map((eachBodega) => {
+        return (
+          
+            <div className="winerie-container" key={eachBodega._id}>
+              <div className="img-winerie-container">
+                <img
+                  className="img-winerie"
+                  src={eachBodega.image}
+                  alt="imagen bodega"
+                />
               </div>
               <div>
-                <Link to={`/bodegas/${eachBodega._id}`}>
-                <h4>{eachBodega.name}</h4>
-                <span>{eachBodega.region}</span>
+                <Link
+                  className="info-winerie"
+                  to={`/bodegas/${eachBodega._id}`}
+                >
+                  <h4 className="winerie-title">{eachBodega.name}</h4>
+                  <span className="winerie-region">{eachBodega.region}</span>
                 </Link>
               </div>
               <br />
             </div>
-          )
-        })
-      }
+       
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Bodegas
+export default Bodegas;
