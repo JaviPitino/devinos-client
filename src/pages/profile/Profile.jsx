28038223846e7bb700133d830012a47cc60a8c4d
Javier López <javierpitino@gmail.com>
2022-12-29 19:35:03 +0100

    responsive edit perfil Ok

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,35 +1,34 @@
-import { useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../context/auth.context'
-import { getProfileDetailsService } from '../../services/auth.service'
-import { NavLink } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
+import { getProfileDetailsService } from "../../services/auth.service";
+import { NavLink } from "react-router-dom";
+import { FiEdit } from "react-icons/fi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 function Profile() {
-
-  const { user } = useContext(AuthContext)
-  const navigate = useNavigate()
-  const [ profileDetails, setProfileDetails ] = useState(null)
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [profileDetails, setProfileDetails] = useState(null);
 
   useEffect(() => {
-    getProfileDetails()
-  }, [])
+    getProfileDetails();
+  }, []);
 
   const getProfileDetails = async () => {
-
     try {
-      const response = await getProfileDetailsService(user._id)
-      
-      setProfileDetails(response.data)
+      const response = await getProfileDetailsService(user._id);
+
+      setProfileDetails(response.data);
       console.log(response.data);
-    } catch(err) {
-      navigate('/error')
+    } catch (err) {
+      navigate("/error");
     }
-  }
-  
+  };
+
   if (!profileDetails) {
-    return <h3>...Loading...</h3>
+    return <h3>...Loading...</h3>;
   }
 
   return (
@@ -38,17 +37,29 @@ function Profile() {
       <h4 className='saludo'> {profileDetails.username[0].toUpperCase() + profileDetails.username.slice(1)}</h4>
       <img className="saludo" src={profileDetails.image} alt="imagen perfil" width={30} /> */}
       {user !== null && (
-        <div className="container-perfil">
-          <h4 className="saludo">
-            {profileDetails.username[0].toUpperCase() + profileDetails.username.slice(1)} 
-          </h4>{" "}
-          <img className="saludo" src={profileDetails.image} alt="imagen perfil" width={50}/>
-        <NavLink to={`/profile/${profileDetails._id}/edit`} className="saludo"><FontAwesomeIcon type='submit' icon={faPenToSquare} /></NavLink> 
-       
+        <div className="container-perfil-second">
+          <h4>
+           <span className="edit-perfil">Bienvenid@: </span>  
+            {profileDetails.username[0].toUpperCase() +
+              profileDetails.username.slice(1)}
+          </h4>
+          <img
+            className="saludo"
+            src={profileDetails.image}
+            alt="imagen perfil"
+          />
+          <NavLink
+            to={`/profile/${profileDetails._id}/edit`}
+            className="edit-perfil-link"
+          >
+            <p className="edit-perfil margin-perfil">Editar perfil</p>
+            
+            <FiEdit className="bookmark" type="submit" style={{ color: '#b8b8b8', width: "1.5em", height: "1.5em" }} />
+          </NavLink>
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
--- src/pages/profile/ProfileEdit.jsx
+++ src/pages/profile/ProfileEdit.jsx
@@ -1,91 +1,117 @@
-import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
-import { editProfileService, getProfileDetailsService } from "../../services/auth.service"
-import uploadService from "../../services/profile.service"
-
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  editProfileService,
+  getProfileDetailsService,
+} from "../../services/auth.service";
+import uploadService from "../../services/profile.service";
+import { BsUpload } from "react-icons/bs";
 
 function ProfileEdit() {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const { id } = useParams()
+  const { id } = useParams();
 
   // **** ESTADOS ****
-  const [ username, setUsername ] = useState("")
-  const [ email, setEmail ] = useState("");
-  const [ image, setImage ] = useState()
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [image, setImage] = useState();
 
   // **** HANDLERS ****
-  const handleUsername = (e) => setUsername(e.target.value)
-  const handleEmail = (e) => setEmail(e.target.value)
+  const handleUsername = (e) => setUsername(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
 
   // **** COMPONENTDIDMOUNT ****
   useEffect(() => {
-    getUserData()
-  }, [])
+    getUserData();
+  }, []);
 
   const getUserData = async () => {
-
     try {
-
-      const response = await getProfileDetailsService()
-      setUsername( response.data.username )
-      setEmail( response.data.email )
-      setImage( response.data.image )
-
-    } catch(err) {
-      navigate("/error")
+      const response = await getProfileDetailsService();
+      setUsername(response.data.username);
+      setEmail(response.data.email);
+      setImage(response.data.image);
+    } catch (err) {
+      navigate("/error");
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
       const updateProfile = {
-        username, email, image
-      }
+        username,
+        email,
+        image,
+      };
 
       await editProfileService(id, updateProfile);
-      navigate("/profile")
-
-    } catch(err) {
-      navigate("/error")
+      navigate("/profile");
+    } catch (err) {
+      navigate("/error");
     }
-  }
+  };
 
   const handleImageChange = async (e) => {
-
-    const uploadForm = new FormData()
-    uploadForm.append("image", e.target.files[0])
+    const uploadForm = new FormData();
+    uploadForm.append("image", e.target.files[0]);
 
     try {
-
       const response = await uploadService(uploadForm);
-      setImage(response.data)
-
-    } catch(err) {
-      navigate("/error")
+      setImage(response.data);
+    } catch (err) {
+      navigate("/error");
     }
-  }
+  };
 
   return (
-    <div>
+    <div className="container-edit">
+      <div className="container-img-edit-label-file">
+        <img className="img-edit-label-file" src={image} alt="imagen perfil" />
+      </div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Usuario: </label>
-        <input type="text" name="username" onChange={handleUsername} value={username} />
-        <br />
-        <label htmlFor="email">Email:</label>
-        <input type="email" name="email" onChange={handleEmail} value={email} />
-        <br />
-        <label htmlFor="image">Imagen: </label>
-        <input type="file" name="image" onChange={handleImageChange} />
-        <img src={image} alt="imagen perfil" width={50} />
-        <br />
-        <button>Actualizar</button>
+        <div className="form-edit-container">
+          <label className="edit-label" htmlFor="username">
+            usuario{" "}
+          </label>
+          <input
+            className="edit-input"
+            type="text"
+            name="username"
+            onChange={handleUsername}
+            value={username}
+          />
+          <label className="edit-label" htmlFor="email">
+            email{" "}
+          </label>
+          <input
+            className="edit-input"
+            type="email"
+            name="email"
+            onChange={handleEmail}
+            value={email}
+          />
+          <div className="container-edit-label-file">
+            <label className="edit-label-file" htmlFor="image">
+              selecciona una imagen &nbsp;&nbsp;
+              <BsUpload />
+              <span className="edit-label-icon"></span>{" "}
+            </label>
+            <input
+              className="edit-input"
+              id="image"
+              type="file"
+              name="image"
+              onChange={handleImageChange}
+            />
+            <button className="btn-edit-perfil">Actualizar</button>
+          </div>
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default ProfileEdit
+export default ProfileEdit;
