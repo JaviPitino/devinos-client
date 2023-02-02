995601b02a5f0856673aed172a71f58114c9d082
Javier López <javierpitino@gmail.com>
2023-02-02 19:12:38 +0100

    add React from react

--- src/pages/bodegas/BodegasCreate.jsx
+++ src/pages/bodegas/BodegasCreate.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import uploadService from "../../services/profile.service";
 import { addNewBodegaService } from "../../services/bodegas.services";
