ebf01d7764447c5f54a90b819b8cbc8d58a33216
Javier López <javierpitino@gmail.com>
2023-02-02 19:32:29 +0100

    adding React for netlify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import Loading from "../components/Loading/Loading";
 import Typewrite from "../components/typewriting/Typewrite";
