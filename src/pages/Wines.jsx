61efc6e70d2a57c9fb4e9b7a45ce35c22b4e746a
Javier López <javierpitino@gmail.com>
2023-02-02 19:05:17 +0100

    cambio import

--- src/pages/Wines.jsx
+++ src/pages/Wines.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { winesListService } from "../services/wines.services";
 import Search from "../components/Search";
